784cbd9ac0074a3002e3d444f1157169463e3a44
Zzl1433 <2446941471@qq.com>
2026-07-16 11:48:31 +0800

    revert: restore cross-platform build configuration

--- config.lua
+++
@@ -1,8 +0,0 @@
--- 添加 Visual Studio 2026 的路径
-add_includedirs("C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.44.35207/include")
-add_includedirs("C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/ucrt")
-add_includedirs("C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/shared")
-add_includedirs("C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/um")
-add_linkdirs("C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.44.35207/lib/x64")
-add_linkdirs("C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/ucrt/x64")
-add_linkdirs("C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um/x64")
